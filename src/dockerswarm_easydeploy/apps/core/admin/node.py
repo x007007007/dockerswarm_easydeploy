18f18b3ce2261e7cf40ae440f05567f47261303a
xxc <x007007007@hotmail.com>
2022-03-19 22:49:26 +0800

    add more node detail config

--- src/dockerswarm_easydeploy/apps/core/admin/node.py
+++ src/dockerswarm_easydeploy/apps/core/admin/node.py
@@ -11,6 +11,7 @@ class NodeModelAdmin(admin.ModelAdmin):
     )
     list_display = (
         'pk',
+        'display_name',
         'uuid',
         'machine_id',
         'group',
@@ -18,6 +19,10 @@ class NodeModelAdmin(admin.ModelAdmin):
         'client_addr',
         'last_update_time',
         'created_time',
+        'is_enable',
+        'is_delete',
+        'is_swarm_node',
+        'is_swarm_manager',
     )
 
     def get_queryset(self, request):
--- src/dockerswarm_easydeploy/apps/core/models/node.py
+++ src/dockerswarm_easydeploy/apps/core/models/node.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class NodeModel(models.Model):
-    uuid = models.CharField(max_length=128)
+    display_name = models.CharField(max_length=64, default='')
+    uuid = models.CharField(max_length=128, unique=True)
     machine_id = models.CharField(max_length=254)
     group = models.ForeignKey("NodeGroupModel", related_name="node_set", on_delete=models.CASCADE)
     node_detail = models.JSONField(null=True, blank=True)
@@ -13,6 +14,26 @@ class NodeModel(models.Model):
     last_update_time = models.DateTimeField(null=True)
     created_time = models.DateTimeField(auto_now_add=True, null=True)
 
+    is_enable = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
+    @property
+    def is_swarm_node(self):
+        if self.swarm_node_id:
+            return True
+        return False
+
+    @property
+    def swarm_node_id(self):
+        return self.node_detail.get('Swarm', {}).get('NodeID')
+
+    @property
+    def is_swarm_manager(self):
+        return self.node_detail.get("Swarm", {}).get("ControlAvailable", False)
+
 
 class NodeGroupModel(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"<{self.__class__.__name__} ({self.pk}) {self.name}>"
