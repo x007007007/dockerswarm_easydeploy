a267951bf6a8beace4228332d460fb79fa35c0f4
xxc <x007007007@hotmail.com>
2022-03-20 16:04:14 +0800

    bug fix

--- src/dockerswarm_easydeploy_client/__main__.py
+++ src/dockerswarm_easydeploy_client/__main__.py
@@ -2,7 +2,10 @@ from .service import serve
 
 
 def main():
-    serve()
+    hub_host = os.environ.get("DOCKERSWARM_EASYDEPLOY_HUB", "dockerswarm-easydeploy")
+    ip = os.environ.get("DOCKERSWARM_EASYDEPLOY_CLIENT_LISTEN_IP", "0.0.0.0")
+    port = os.environ.get("DOCKERSWARM_EASYDEPLOY_CLIENT_LISTEN_PORT", "15005")
+    serve(hub_host, address=ip, port=port)
 
 
 if __name__ == '__main__':
